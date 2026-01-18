d30641e2cc296613395d0fc10e76ba442ff39769
sirmaelstrom <jmheath@gmail.com>
2026-01-18 13:42:03 -0600

    feat: add MelonMod entry point with initialization logging

---
+++ src/ModMain.cs
@@ -0,0 +1,45 @@
+using MelonLoader;
+using UnityEngine;
+
+#if IL2CPP
+using Il2CppScheduleOne;  // Verify this namespace with dnSpy!
+using Il2CppInterop.Runtime.Injection;
+#else
+using ScheduleOne;  // Verify this namespace with dnSpy!
+#endif
+
+[assembly: MelonInfo(typeof(ScheduleIMod.ModMain), "ScheduleIMod", "0.1.0", "YourName")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace ScheduleIMod
+{
+    public class ModMain : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("=================================");
+            LoggerInstance.Msg("Schedule I Mod Loaded!");
+
+            #if IL2CPP
+            LoggerInstance.Msg("Running IL2CPP build");
+            #else
+            LoggerInstance.Msg("Running Mono build");
+            #endif
+
+            LoggerInstance.Msg($"MelonLoader Version: {BuildInfo.Version}");
+            LoggerInstance.Msg($"Game Version: {Application.version}");
+            LoggerInstance.Msg($"Unity Version: {Application.unityVersion}");
+            LoggerInstance.Msg("=================================");
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            LoggerInstance.Msg("Late initialization complete - patches should be active");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            LoggerInstance.Msg($"Scene loaded: {sceneName} (index: {buildIndex})");
+        }
+    }
+}
